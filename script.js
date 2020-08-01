bf355774d0e20050a25413455f8a3ace706440cb
Daniel Gerrald <daniel.gerrald@icloud.com>
2020-07-31 20:56:13 -0400

    Added variables to script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialC = ["!", "@", "#", "$", "%", "^", "&", "?"];
+var LowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var UpperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var passLength = "";
+var wantNumber = false;
+var wantSpecialC = false;
+var wantLowerC = false;
+var wantUpperC =false;
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
