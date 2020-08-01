bb976435ccf4650885fb79b3f9ddc9a13f6d47a3
Daniel Gerrald <daniel.gerrald@icloud.com>
2020-08-01 19:08:10 -0400

    updated script.js

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Password_Generator
+Description
+In this project I will create a responsive web page that prompts the generates a random password. When the generate button is clicked the user will be prompted to select a number between and including 8 - 128. If the user selects cancel then the process will end. If the user enters anything other than a number that is between 8 and 128 including 8 and 128 then they will be prompted to try again.
+
+The user will then be prompted to confirm if they would like to include numbers in their password by selecting ok. If they select cancel then numbers will not be used.
+This process will prompt again to confirm special character as well as uppercase letters and lowercase letters.
+
+If a user selects cancel for all four of these prompts then the user will be prompted that they must select at least one option and to try again.
+
+The java script code will then combine the arrays of numbers, special character, uppercase letters and lowercase letters dependent uppon the users confirmations from the previous prompts. 
+
+Then a random element will be selected from the combined array the number of times the user input from the first prompt and added to a variable. 
+
+This variable will then be output in the text box for the user to see.
--- script.js
+++ script.js
@@ -8,6 +8,75 @@ var wantSpecialC = false;
 var wantLowerC = false;
 var wantUpperC =false;
 
+function generatePassword() {
+    passLength = (prompt("How many characters long would you like your password to be? \nPlease choose a number between 8 and 128"));
+    if (passLength === null) {
+      alert("Good Bye!!");
+      return;
+    }
+    
+//console.log(Length)
+  while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+      alert("Password must be between 8 and 128 characters long. \nPlease Try again.");
+      passLength = (prompt("How many characters long would you like your password to be? \nPlease choose a number between 8 and 128."));
+      if (passLength === null) {
+        alert("Good Bye!!");
+        return;
+      }
+      passLength = parseInt(passLength);
+    }
+    alert("Your password will have " + passLength + " characters.");
+
+    var allTrue = confirm("Would you like your password to include ALL of the following:\n-Numbers \n-Special Charecters \n-Lowercase Letters \n-Uppercase Letters \nClick Cancel if you would like to select other charecter options.");
+    if (allTrue) {
+      wantNumber = true;
+      wantSpecialC = true;
+      wantLowerC = true;
+      wantUpperC =true;
+    }
+    else {
+        wantNumber = confirm("Click OK if you would like to include numbers. \nClick Cancel if you do not wish to include numbers.");
+        wantSpecialC = confirm("Click OK if you would like to include special characters. \nClick Cancel if you do not wish to include special characters.");
+        wantLowerC = confirm("Click OK if you would like to include lower case letters. \nClick Cancel if you do not wish to include lower case letters.");
+        wantUpperC = confirm("Click OK if you would like to include upper case letters. \nClick Cancel if you do not wish to include upper case letters.");
+      
+      while (wantNumber === false && wantSpecialC === false && wantLowerC === false && wantUpperC === false) {
+        alert("You must choose at least one option.");
+        wantNumber = confirm("Click OK if you would like to include numbers. \nClick Cancel if you do not wish to include numbers.");
+        wantSpecialC = confirm("Click OK if you would like to include special characters. nClick Cancel if you do not wish to include special characters.");
+        wantLowerC = confirm("Click OK if you would like to include lower case letters. \nClick Cancel if you do not wish to include lower case letters.");
+        wantUpperC = confirm("Click OK if you would like to include upper case letters. \nClick Cancel if you do not wish to include upper case letters.");
+      }
+    }
+
+
+  var passwordC = [];
+  var newPassword = "";
+
+  if (wantNumber) {
+    passwordC.push.apply(passwordC, number);
+  }
+
+  if (wantSpecialC) {
+    passwordC.push.apply(passwordC, specialC);
+  }
+
+  if (wantLowerC) {
+    passwordC.push.apply(passwordC, LowerC);
+  }
+
+  if (wantUpperC) {
+    passwordC.push.apply(passwordC, UpperC);
+  }
+
+//console.log(passwordC)
+
+  for (var i = 0; i < passLength; i++) {
+    newPassword = newPassword + passwordC[Math.floor(Math.random() * passwordC.length)];
+  }
+//console.log(newPassword)
+  return newPassword;
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -23,3 +92,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
